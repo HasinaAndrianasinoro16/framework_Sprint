86a1b5ea99342aa858fbe3f1c2ddf8acf04188ee
HasinaAndrianasinoro16 <120100370+HasinaAndrianasinoro16@users.noreply.github.com>
2023-07-07 23:32:56 +0300

    sprint 10

---
+++ Sprint10/MyFrameWork/src/_compilation.bat
@@ -0,0 +1,3 @@
+cd "D:\L2\apache-tomcat-8\webapps\fmrwk\Sprint10\MyFrameWork"
+javac -parameters -cp ".\WEB-INF\lib\*" -d .\WEB-INF\classes src\*.java
+cd "D:\L2\apache-tomcat-8\webapps\fmrwk\Sprint10\MyFrameWork\src"
